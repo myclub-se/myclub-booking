450ee93b066605796655467a8e0baea0ec266299
Andréas Kühne <andreas@kuhne.se>
2025-03-25 15:57:46 +0100

    Updated plugin file

--- myclub-booking.php
+++ myclub-booking.php
@@ -14,3 +14,36 @@ Text Domain: myclub-booking
 Domain Path: /languages
 License: GPLv2 or later
 */
+
+defined( 'ABSPATH' ) or die( 'Access denied' );
+
+if ( version_compare( PHP_VERSION, '7.4', '<' ) ) {
+    exit( "This plugin requires PHP 7.4 or higher. You're still on PHP " . PHP_VERSION );
+}
+
+if ( file_exists( plugin_dir_path( __FILE__ ) . '/lib/autoload.php' ) ) {
+    require_once( plugin_dir_path( __FILE__ ) . '/lib/autoload.php' );
+}
+
+define( 'MYCLUB_BOOKING_PLUGIN_VERSION', '0.0.1' );
+
+function myclub_booking_activate()
+{
+}
+
+// Register activation code
+register_activation_hook( __FILE__, 'myclub_booking_activate' );
+
+function myclub_booking_deactivate()
+{
+}
+
+// Register deactivation code
+register_deactivation_hook( __FILE__, 'myclub_booking_deactivate' );
+
+function myclub_booking_uninstall()
+{
+}
+
+// Register uninstall code
+register_uninstall_hook( __FILE__, 'myclub_booking_uninstall' );
